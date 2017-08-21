7fd1d6ba607abde8637ba58b68386f7516f5bb8d
techcrab101 <techcrab101@gmail.com>
2017-08-21 07:36:36 -0400

    Added notebook

--- test.py
+++ test.py
@@ -1,37 +1,6 @@
 import numpy as np
 from termcolor import colored
 
-def show_init_cond():
-    '''prints the initial conditions'''
-    print(colored('participant matrix:', 'cyan'))
-    print(p_mat)
-    print()
-
-    print(colored('expected resultant matrix:', 'cyan'))
-    print(exp_mat)
-    print()
-    
-    #print(colored('expected resultant matrix ranked least to greatest:', 'cyan'))
-    #print(rank_exp_mat)
-    #print()
-
-    print(colored('currency heuristic matrix:', 'green'))
-    print(curr_heur_mat)
-    print()
-    
-    print(colored('object heuristic matrix:', 'green'))
-    print(obj_heur_mat)
-    print()
-
-    print(colored('resultant matrix:', 'green'))
-    print(res_mat)
-    print()
-    
-    #print(colored('resultant matrix ranked least to greatest:', 'green'))
-    #print(rank_mat)
-    #print()
-    pass
-    
 p_count = 3
 curr_type_count = 2
 
@@ -39,6 +8,14 @@ curr_type_count = 2
 p_mat = np.arange(6).reshape(curr_type_count, p_count)
 exp_mat = np.array([4,3,1], dtype=float)
 rank_exp_mat = exp_mat.argsort()
+
+print(colored('participant matrix:', 'cyan'))
+print(p_mat)
+print()
+
+print(colored('expected resultant matrix:', 'cyan'))
+print(exp_mat)
+print()
 
 # Algorithm
 curr_heur_mat = np.ones([1, curr_type_count], dtype=float)
@@ -48,7 +25,25 @@ res_mat = np.dot(curr_heur_mat, obj_heur_mat * p_mat)
 
 rank_mat = res_mat.argsort()
 
-show_init_cond()
+#print(colored('expected resultant matrix ranked least to greatest:', 'cyan'))
+#print(rank_exp_mat)
+#print()
+
+print(colored('currency heuristic matrix:', 'green'))
+print(curr_heur_mat)
+print()
+
+print(colored('object heuristic matrix:', 'green'))
+print(obj_heur_mat)
+print()
+
+print(colored('resultant matrix:', 'green'))
+print(res_mat)
+print()
+
+#print(colored('resultant matrix ranked least to greatest:', 'green'))
+#print(rank_mat)
+#print()
 
 # Learning
 # Optimizing for expected matrix
@@ -63,12 +58,21 @@ print(error_mat)
 print()
 
 curr_cont_mat = p_mat/p_mat.sum(axis=0)
-obj_cont_mat = p_mat/p_mat.sum(axis=1).reshape(curr_type_count,1)
 
 print(colored('currency cont mat', 'yellow'))
 print(curr_cont_mat)
 print()
 
+obj_cont_mat = p_mat/p_mat.sum(axis=1).reshape(curr_type_count,1)
+
 print(colored('obj cont mat', 'yellow'))
 print(obj_cont_mat)
 print()
+
+cont_mat = (curr_cont_mat + obj_cont_mat)/2
+
+print(colored('overal contribution matrix', 'yellow'))
+print(cont_mat)
+print()
+
+
