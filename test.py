a49788068ef2dbe16cebb1570ef106855a74b7c3
techcrab101 <techcrab101@gmail.com>
2017-08-22 19:46:43 -0400

    Added new learning rate for currency heuristics. This fixed previous issues with scalability

--- test.py
+++ test.py
@@ -3,11 +3,7 @@ from termcolor import colored
 import time
 import math
 
-def correct(error_mat, p_mat, obj_heur_mat, curr_heur_mat):
-    print(colored('error matrix', 'yellow'))
-    print(error_mat)
-    print()
-    
+def correct(error_mat, obj_learning_rate, curr_learning_rate, p_mat, obj_heur_mat, curr_heur_mat):
     curr_cont_mat = p_mat/p_mat.sum(axis=0)
     
     obj_cont_mat = p_mat/p_mat.sum(axis=1).reshape(curr_type_count,1)
@@ -22,20 +18,29 @@ def correct(error_mat, p_mat, obj_heur_mat, curr_heur_mat):
     
     x /= z
     y /= z
+   
+    print('x and y')
+    print(x)
+    print(y)
+    print()
+
+    obj_heur_mat += (error_mat * obj_learning_rate)* y
     
-    obj_heur_mat += error_mat * y
-    
-    z = error_mat.sum()
-    
-    curr_heur_mat += x*z
+    j = error_mat.sum() * curr_learning_rate
+   
+    print('j')
+    print(j)
+    print()
+
+    curr_heur_mat += x*j
     
     res_mat = np.dot(curr_heur_mat, obj_heur_mat * p_mat)
     
     rank_mat = res_mat.argsort()
     
-    return res_mat, obj_heur_mat, curr_heur_mat, learning_rate
+    return res_mat, obj_heur_mat, curr_heur_mat
 
-p_count = 10
+p_count = 20
 curr_type_count = 5
 
 # Training Data
@@ -74,28 +79,28 @@ print()
 
 # Learning
 # Optimizing for expected matrix
-learning_rate = .02 #1/(p_count + curr_type_count) # Max learning rate
-min_learning_rate = .001
-for i in range(500):
+obj_learning_rate = .02 #1/(p_count + curr_type_count) # Max learning rate
+curr_learning_rate = .001
+
+for i in range(100):
     print(colored('i:', 'red'), i)
     print()
     
     error_mat = exp_mat - res_mat
-    print('error mat::')
+    print(colored('error mat', 'yellow'))
     print(error_mat)
-
-    #print('calculated learning rate')
-    #cal_lr = (learning_rate * (np.absolute(error_mat).sum()/len(error_mat))) / initial_err 
-   
-    #if cal_lr > learning_rate or math.isnan(cal_lr):
-    #    cal_lr = learning_rate
-
-    #print(cal_lr)
+    print()
     
-    #error_mat *= cal_lr
-    error_mat *= learning_rate
-    learning_rate *= .8
-    res_mat, obj_heur_mat, curr_heut_mat, learning_rate = correct(error_mat, p_mat, obj_heur_mat, curr_heur_mat)
+    print(colored('object learning rate', 'yellow'))
+    print(obj_learning_rate)
+    print()
+
+    print(colored('curr learning rate', 'yellow'))
+    print(curr_learning_rate)
+    print()
+
+    res_mat, obj_heur_mat, curr_heut_mat = correct(error_mat, obj_learning_rate , curr_learning_rate, 
+            p_mat, obj_heur_mat, curr_heur_mat)
 
     print(colored('currency heuristic matrix:', 'green'))
     print(curr_heur_mat)
