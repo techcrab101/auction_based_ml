0d283a4492980617aced14f355b372d98aebe843
techcrab101 <techcrab101@gmail.com>
2018-02-14 07:36:13 -0500

    Made cleaner version of working auction alg

--- auction_alg.py
+++ auction_alg.py
@@ -49,31 +49,92 @@ class Object():
             )
         return x
 
-def display_status(people, objects):
-    print(colored('###########################', 'white'))
-    for i in list(people.values()):
-        print(colored('The People', 'green'))
-        stat = i.get_status()
-        print(stat)
-        print()
-    for i in list(objects.values()):
-        print(colored('The Objects', 'yellow'))
-        stat = i.get_status()
-        print(stat)
-        print()
-
 def get_max_ben(person, objects):
-    return max(map(lambda x: [people[i].get_benefit(x), x], list(objects.values())), key=lambda y: y[0])
+    ben_ID_list = map(lambda x: [person.get_benefit(x), x], list(objects.values()))
+    return max(ben_ID_list, key=lambda y: y[0])
 
-def check_happiness(people, objects):
-    for i in (people.values()):
-        if not(i.happy):
-            return False
+class auction_alg():
+    def __init__(self, people, objects, epsilon=None):
+        self.people = people
+        self.objects = objects
+        if epsilon is None:
+            self.epsilon = 1/(len(people) + 1)
+        else:
+            self.epsilon = epsilon
 
-    return True
+    def display_status(self):
+        print(colored('###########################', 'white'))
+        for i in list(self.people.values()):
+            print(colored('The People', 'green'))
+            stat = i.get_status()
+            print(stat)
+            print()
+        for i in list(self.objects.values()):
+            print(colored('The Objects', 'yellow'))
+            stat = i.get_status()
+            print(stat)
+            print()
+
+    def check_happiness(self):
+        for i in (self.people.values()):
+            if not(i.happy):
+                return False
+        return True
+
+    def calculate_happiness(self):
+        for i in range(len(self.people)):
+            best = get_max_ben(self.people[i], objects)
+            if best[0] - self.epsilon <= self.people[i].benefit:
+                self.people[i].max_pot_ben = self.people[i].benefit
+                self.people[i].max_pot_obj = self.people[i].object
+                self.people[i].happy = True
+                continue
+            self.people[i].max_pot_ben = best[0]
+            self.people[i].max_pot_obj = best[1].ID
+            self.people[i].happy = True if self.people[i].max_pot_ben == self.people[i].benefit else False  
+
+    def perform_auction_step(self):
+        unhappy_people = filter(lambda x: x.happy is False, list(self.people.values()))
+        unhappy_people_ID = list(map(lambda y: y.ID, unhappy_people))
+        print(len(unhappy_people_ID))
+
+        person_i = self.people[unhappy_people_ID[0]]
+        object_i = self.objects[person_i.object]
+
+        object_j = self.objects[person_i.max_pot_obj]
+        person_j = self.people[object_j.person]
+
+        nij = person_i.get_benefit(object_j)
+
+        cpy_objects = copy.deepcopy(self.objects)
+        del cpy_objects[person_i.max_pot_obj]
+
+        a = get_max_ben(person_i, cpy_objects)
+
+        nij2 = person_i.get_benefit(a[1])
+
+        xj = nij - nij2 + self.epsilon
+        object_j.cost = object_j.get_costs() + xj
+        person_i.set_pairing(object_j)
+        person_j.set_pairing(object_i)
+
+        object_j.person = person_i.ID
+        object_i.person = person_j.ID
+
+        self.people[person_i.ID] = person_i
+        self.people[person_j.ID] = person_j
+        self.objects[object_i.ID] = object_i
+        self.objects[object_j.ID] = object_j
+        
+        # recalculate happiness measure
+        self.calculate_happiness()
+
+    def perform_auction_process(self):
+        while not(self.check_happiness()):
+            self.perform_auction_step()
+            self.display_status()
 
 y = 10
-epsilon = 1/(y+1)
 
 people = {}
 objects = {}
@@ -90,52 +151,6 @@ for i in range(y):
     people[i].max_pot_obj = best[1].ID
     people[i].happy = True if people[i].max_pot_ben == people[i].benefit else False  
 
-display_status(people, objects)
+auction_alg = auction_alg(people,objects)
 
-while not(check_happiness(people, objects)):
-    unhappy_people_ID = list(map(lambda y: y.ID, filter(lambda x: x.happy is False, list(people.values()))))
-    pOI = unhappy_people_ID[0]
-
-    person_i = people[pOI]
-    object_i = objects[person_i.object]
-
-    object_j = objects[person_i.max_pot_obj]
-    person_j = people[object_j.person]
-
-
-    nij = person_i.get_benefit(object_j)
-
-    cpy_objects = copy.deepcopy(objects)
-    del cpy_objects[person_i.max_pot_obj]
-
-    a = get_max_ben(person_i, cpy_objects)
-
-    nij2 = person_i.get_benefit(a[1])
-
-    xj = nij - nij2 + epsilon
-    object_j.cost = object_j.get_costs() + xj
-    person_i.set_pairing(object_j)
-    person_j.set_pairing(object_i)
-
-    object_j.person = person_i.ID
-    object_i.person = person_j.ID
-
-    people[person_i.ID] = person_i
-    people[person_j.ID] = person_j
-    objects[object_i.ID] = object_i
-    objects[object_j.ID] = object_j
-    
-    # recalculate happiness measure
-
-    for i in range(len(people)):
-        best = get_max_ben(people[i], objects)
-        if best[0] - epsilon <= people[i].benefit:
-            people[i].max_pot_ben = people[i].benefit
-            people[i].max_pot_obj = people[i].object
-            people[i].happy = True
-            continue
-        people[i].max_pot_ben = best[0]
-        people[i].max_pot_obj = best[1].ID
-        people[i].happy = True if people[i].max_pot_ben == people[i].benefit else False  
-
-    display_status(people, objects)
+auction_alg.perform_auction_process()
